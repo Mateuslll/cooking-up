debc761ce4dd43065d82a806707ad801dd1e4738
gellado <lima.mt96@gmail.com>
2024-05-06 07:52:40 -0300

    Create componentes e estilos em css

--- src/components/SelecionarIngredientes.vue
+++ src/components/SelecionarIngredientes.vue
@@ -1,17 +1,19 @@
 <script lang="ts">
 import { obterCategorias } from '@/http/index';
 import type ICategoria from '@/interfaces/ICategoria';
-    export default {
-        data(){
-            return{
-                categorias:[] as ICategoria []
-            }
-        },
-        //metodo de ciclo de vida executado depois que o data estive definido
-        async created(){
-            this.categorias = await obterCategorias();
-        }
-    }
+import CardCategoria from './CardCategoria.vue';
+export default {
+    data() {
+        return {
+            categorias: [] as ICategoria[]
+        };
+    },
+    //metodo de ciclo de vida executado depois que o data estive definido
+    async created() {
+        this.categorias = await obterCategorias();
+    },
+    components: { CardCategoria }
+}
 </script>
 
 <template>
@@ -22,19 +24,15 @@ import type ICategoria from '@/interfaces/ICategoria';
             Selecione abaixo os ingredientes que você quer usar nesta receita:
         </p>
         <ul class="categorias">
-            <li v-for="categoria in categorias" :key="categoria.nome" >
-                {{ categoria.nome }}
+            <li v-for="categoria in categorias" :key="categoria.nome">
+                <CardCategoria :categoria="categoria"/>
             </li>
         </ul>
         <p class="paragrafo dica">
             *Atenção esperamos que voce tenha em casa sal, pimenta e água.
         </p>
     </section>
-
 </template>
 
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
